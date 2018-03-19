1f547b53688ed526456ea4b873ce53f041e2900a
Pratik Kumar <pratikyoff@gmail.com>
2018-03-20 00:08:30 +0530

    A simple web socket server example which takes in a message and relays it to every other connection

---
+++ src/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+
+var upgrader = websocket.Upgrader{}
+
+//Message struct to encapsulate a message
+type Message struct {
+	Content string `json:"content"`
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("../public"))
+	http.Handle("/", fs)
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleMessages()
+
+	log.Println("server started on localhost :8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+	clients[ws] = true
+	for {
+		var msg Message
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Println(err)
+			delete(clients, ws)
+			break
+		}
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Println(err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
